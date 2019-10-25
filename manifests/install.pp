36081bbfc26978a1526842125184437176509f6a
Dereck Martin <dereckmartin@users.noreply.github.com>
2019-10-25 01:36:15 -0400

    Updated repo configuration
    Grafana updated repo link: https://grafana.com/docs/installation/rpm/

--- manifests/install.pp
+++ manifests/install.pp
@@ -6,12 +6,12 @@ class grafana::install {
       'RedHat': {
         yumrepo { 'grafana':
           descr         => 'Grafana',
-          baseurl       => "https://packagecloud.io/grafana/stable/el/${::operatingsystemmajrelease}/\$basearch",
-          repo_gpgcheck => true,
-          enabled       => true,
-          gpgcheck      => true,
-          gpgkey        => 'https://packagecloud.io/gpg.key https://grafanarel.s3.amazonaws.com/RPM-GPG-KEY-grafana',
-          sslverify     => true,
+          baseurl       => "https://packages.grafana.com/oss/rpm",
+          repo_gpgcheck=1
+          enabled=1
+          gpgcheck=1
+          gpgkey=https://packages.grafana.com/gpg.key
+          sslverify=1
           proxy         => $::grafana::proxy,
           before        => Package['grafana'],
         }
